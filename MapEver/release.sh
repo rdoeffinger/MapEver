4307980f036997e9015a395c227d05699a5913f8
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2016-11-16 22:11:22 +0100

    Another version bump.
    Also add a release script.

---
+++ MapEver/release.sh
@@ -0,0 +1,5 @@
+VER=$(grep versionName AndroidManifest.xml | sed -e 's/.*"\(.*\)".*/v\1/')
+cp build/outputs/mapping/release/mapping.txt mapping-$VER.txt
+cp MapEver-release.apk MapEver-$VER.apk
+gpg -a --detach-sign MapEver-$VER.apk
+git tag -u 06D4D9C7 "$VER"
